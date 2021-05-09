4f80942ee346262c0377644647e83c68be3533ac
Marcin Kamiński <kontakt@mkaminski.net>
2021-05-09 11:07:37 +0200

    Initial commit

---
+++ src/main/java/net/mkaminski/sri4soap/Sri4SoapApplication.java
@@ -0,0 +1,13 @@
+package net.mkaminski.sri4soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sri4SoapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sri4SoapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/mkaminski/sri4soap/Sri4SoapApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mkaminski.sri4soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sri4SoapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
